c4e389bcb2c3ede57ee9374fc02c1bd8bdfee59f
lasithatharanga <lasitha119@gmail.com>
2016-09-18 21:59:41 +0530

    Form validation error display debug

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,7 +9,7 @@
            <h1>Create Post</h1>
            @if(count($errors)>0)
            <div class="alert alert-danger">
-               @foreach($errors->all as $error)
+               @foreach($errors->all() as $error)
                <p>{{$error}}</p>
                @endforeach
            </div>
